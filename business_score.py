1d0a9f28758c1ef0a45d7f8d4f3b6b2dba77da90
Lynda-bssh <lynda.hadjemi@gmail.com>
2023-12-01 16:45:06 +0100

    add function busines score

---
+++ business_score.py
@@ -0,0 +1,9 @@
+from sklearn.metrics import confusion_matrix
+
+
+def custom_score(y_actual, y_pred):
+    
+    tp,fp,fn,tn = confusion_matrix(y_actual, y_pred).ravel()
+    cost = 10*fn + fp 
+
+    return float(cost)
