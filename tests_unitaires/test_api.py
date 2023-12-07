e1a0bc0d2cd14a3dbe26d448c772061de9657553
Lynda-bssh <lynda.hadjemi@gmail.com>
2023-12-07 13:58:37 +0100

    add test_unitaire

--- tests_unitaires/test_api.py
+++ tests_unitaires/test_api.py
@@ -1,18 +1,29 @@
-import pytest
+
 import requests
 from bs4 import BeautifulSoup
 import sys
+import json
+import os
 from pathlib import Path
 from flask import Flask, request, jsonify, render_template
+
 parent_directory = Path(__file__).resolve().parent.parent
 api_directory = parent_directory / "api"
 sys.path.append(str(api_directory))
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'api')))
 from api import app
+import pytest
 
+### creation d'un client web:
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
 
 
 ### test qui permet de tester la base route de l'api qui plante vers base.html:
-def test_base_route():
+def test_base_route(client):
     url = 'http://127.0.0.1:5000/'
     client = app.test_client()
     response = client.get(url)
@@ -23,41 +34,49 @@ def test_base_route():
     assert response.status_code == 200
 
 
-#   pytest -v test_api.py::test_client_ids_route
 ### verifier la route de recuperation des ID_client(sk_id_curr):
-def test_client_ids_route():
-
-    url = 'http://127.0.0.1:5000/reference_list'  
-    response = requests.get(url)
+def test_client_ids_route(client):
+    response = client.get('/reference_list')  
     assert response.status_code == 200
-    data = response.json()
-    # Vérifiez que 'data' est une chaîne JSON valide
-    assert 'SK_ID_CURR' in data
+
+    # Renommez la variable 'json' à 'data_response'
+    data_response = json.loads(response.get_data(as_text=True))  
+    assert 'SK_ID_CURR' in data_response
 
 
-### tester si cette clé est bien dans response.json:
-def test_id_client_in_json():
 
-    url = 'http://127.0.0.1:5000/reference_list'  
-    response = requests.get(url)
+#   pytest -v test_api.py::test_client_ids_route
+# ### tester si cette clé est bien dans response.json:
+def test_id_client_in_json(client):
+
+    response = client.get('/reference_list')
+    
     assert response.status_code == 200
-    data = response.json()
-    # Vérifiez que 'data' est une chaîne JSON valide
-    assert '14525' in data
+    # on va s'assurer que la reponse et en json
+    assert response.headers['Content-Type'] == 'application/json'
+    # Obtenez les données JSON de la réponse
+    data_response = response.get_json()
+    # Vérifiez que '14525' est présent dans les données JSON
+    assert '14525' in data_response
 
 
-### test pour verifier la prediction:
-def test_predict_route():
 
-    url = 'http://127.0.0.1:5000/predict'  
-    sk_id_curr = 150224
-    data = {'SK_ID_CURR': sk_id_curr}
-    response = requests.post(url, data=data)
-    assert response.status_code == 200
-    data = response.json()
-    ### verifier si la probabilité de risque est dans data collectée:
-    assert 'probability' in data
-   
+
+
+# # ### test pour verifier la prediction:
+# def test_predict_route(client):
+#     sk_id_curr = 150224
+#     data = {'SK_ID_CURR': sk_id_curr}
+#     response = client.post('/predict', data=data)
+
+#     assert response.status_code == 200
+#     response_data = response.json()
+
+#     # Vérifier si la clé 'probability' est présente dans les données
+#     assert 'probability' in response_data
+
+
+
   
 
 
