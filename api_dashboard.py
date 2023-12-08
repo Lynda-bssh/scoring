ba4e89e490bbf0ab6b2ab33858202e2b5bdd8601
Lynda-bssh <lynda.hadjemi@gmail.com>
2023-12-08 20:56:26 +0100

    update api-dash

--- api_dashboard.py
+++ api_dashboard.py
@@ -1,9 +1,11 @@
 import subprocess
 import os
 
+
+
 # Chemin relatif pour accéder à api.py et dashboard.py
-scripts_directory_api = "./api"
-scripts_directory_dashboard = "./streamlit"
+# scripts_directory_api = "scoring/api"
+# scripts_directory_dashboard = "scoring/streamlit"
 
 # Obtenez les variables d'environnement actuelles
 env = os.environ.copy()
@@ -12,7 +14,10 @@ env = os.environ.copy()
 env["PORT"] =  "5000"
 # executer code 
 # Exécutez api.py avec python avec la nouvelle variable d'environnement
-subprocess.Popen(["python", f"{scripts_directory_api}/api.py"], env=env)
+subprocess.Popen(["pwd"] ,shell = True )
+subprocess.Popen(["ls"] ,shell = True )
+subprocess.Popen(["python ./app/api.py"] ,shell = True )
 
 # Exécutez dashboard.py avec streamlit
-subprocess.Popen(["python -m streamlit", "run", f"{scripts_directory_dashboard}/my_dashboard.py", "--server.port", "8080"], env=env)
+subprocess.Popen(["python -m streamlit run ./dashboard/my_dashboard.py"",--server.port", "8080"], shell=True)
+# "--server.port", "8080"
