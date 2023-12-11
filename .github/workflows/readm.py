b41d978abe899c28f729bc5489ad96ed8aaeab0c
Lynda-bssh <lynda.hadjemi@gmail.com>
2023-12-11 10:10:25 +0100

    add file .github/workflows/readm.py

---
+++ .github/workflows/readm.py
@@ -0,0 +1,8 @@
+
+
+
+
+
+   az ad sp create-for-rbac --name "scoring-pret-a-depenser_lynda_hadjemi" --role contributor \
+                            --scopes /subscriptions/0454dfb8-7cbd-4653-8010-9f6d59d08ea8 --sdk-auth
+                           
